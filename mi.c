5c919a11da016b29ac2302b62e05e15f8f63c7f4
FaDyHaNy04 <142107443+FaDyHaNy04@users.noreply.github.com>
2023-08-25 19:41:44 +0200

    sumtow

---
+++ mi.c
@@ -0,0 +1,16 @@
+int sum=0;
+for (int i=0;i<=num;i++){
+sum =sum+i;
+
+}
+return sum;
+}
+
+int main(){
+int res,num;
+printf("enter the number: ");
+scanf("%d",&num);
+res=summax(num);
+printf("%d",res);
+
+}
